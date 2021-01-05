8d1dab5e4a32e92e1d20de50165feba60dfd4658
Short <lun4.short@gmail.com>
2021-01-05 22:12:44 +0900

    .

--- Nb/post.js
+++ Nb/post.js
@@ -1,6 +1,10 @@
 t("post",function(m,app){
+	var i=0;
 	m.reload=function(){
-		m.css=app.css("post");
-		m.lang=app.cssl("post");
+		m.css=app.css("post",load);
+		m.lang=app.cssl("post",load);
 	};
+	function load(){
+		if(i++>0)m.show();
+	}
 });
