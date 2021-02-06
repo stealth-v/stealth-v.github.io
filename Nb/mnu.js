ee2d04c00ed3ba3a57b21c94dbd328b15412f418
Short <lun4.short@gmail.com>
2021-02-06 18:25:27 +0900

    .

--- Nb/mnu.js
+++ Nb/mnu.js
@@ -11,7 +11,7 @@ xu.t("mnu",function(m,app){
 		self={on:on,freg:null,
 		};
 		sc.onsubmit=function(){
-			if(app.content){
+			if(app.content&&app.content.page){
 				var a=this.querySelector("[type=search]"),b;
 				if(b=a.value){
 					if(this.querySelector(":checked").value)b+="&s=1";
