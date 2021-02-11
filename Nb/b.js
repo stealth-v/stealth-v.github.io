0d68a54dec0d868305f6b9f1b4ed762a8c305731
Short <lun4.short@gmail.com>
2021-02-11 14:41:04 +0900

    .

--- Nb/b.js
+++ Nb/b.js
@@ -6,7 +6,7 @@ xu.r(function(v){
 		},"https://b.apx.kr");
 	};
 	parent.postMessage({
-		path:location.pathname.replace(/.[^\/]+/,""),
+		href:location.href.replace(/.[^\/]+/,""),
 		title:document.title,
 		favicon:document.querySelector("[rel=icon]").href
 	},"https://b.apx.kr");
