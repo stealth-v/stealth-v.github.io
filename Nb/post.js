dba5ff0b2cc58d0510b334d378f412c794a9bf8e
Short <lun4.short@gmail.com>
2021-02-18 16:56:55 +0900

    .

--- Nb/post.js
+++ Nb/post.js
@@ -85,7 +85,7 @@ xu.t("post",function(m,app){
 				switch(p.tagName){
 				case "ARTICLE":
 					a=p.querySelector("a");
-					if(a.textContent)a.click();
+					if(a.textContent)open(a.href+".ps","_self");
 					return;
 				}
 			}
