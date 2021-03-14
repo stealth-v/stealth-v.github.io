6cdac4d8147d6aed5cb61b308133c49a13607d61
Short <lun4.short@gmail.com>
2021-03-14 19:30:31 +0900

    .

--- signin.js
+++ signin.js
@@ -2,16 +2,24 @@ xu.t("signin",function(m,app){
 	m.exports=function(){
 		var content=this,
 		on={
-			
+
 		};
 		return self;
 	};
 
-	var css;
+	var css,jsbn,rsa;
 	m.task=function(fin){
-		css=app.css("signin",fin);
+		css=app.css("signin",done);
+		jsbn=app.js("jsbn",done);
+		rsa=app.js("rsa",done);
+
+		function done(){
+			if(css&&jsbn&&rsa)fin();
+		}
 	};
 	m.unload=function(){
 		xu.stylesheet.delete(css);
+		xu.script.delete(jsbn);
+		xu.script.delete(rsa);
 	};
 })
