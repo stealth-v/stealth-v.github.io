5958eeb6fc63ccf7648276e1ed1e604a1d778235
Short <lun4.short@gmail.com>
2021-03-24 08:35:54 +0900

    .

--- Nb/mnu.js
+++ Nb/mnu.js
@@ -2,26 +2,13 @@ xu.t("mnu",function(m,app){
 	m.exports=function(){
 		var content=this,
 		sc=this.querySelector(".sc"),
-		usr=this.querySelector(".usr"),
 		on={
 			sc:function(){
 				xu.toggle(sc);
 				xu.focus(sc.querySelector("[type=search"));
-			},
-			usr:function(){
-				if(xu.peek(usr))xu.toggle(usr);
-				else app.sign_state(app.sign_state1,"q",usr);
 			}
 		},
-		self={on:on,freg:null,
-			reload:function(){
-				var a=app.get_profile();
-				if(a){
-					usr.style.background=a[2];
-				}else{
-					usr.removeAttribute("style");
-				}
-			}
+		self={on:on,freg:null
 		};
 
 		this.onclick=function(e){
--- mnu.js
+++ mnu.js
@@ -1,8 +1,25 @@
 xu.t("mnu",function(m,app){
 	m.exports=function(){
 		var content=this,
-		on={},
-		self={on:on,freg:null
+		usr=this.querySelector(".usr"),
+		on={
+			usr:function(){
+				if(xu.peek(usr))xu.toggle(usr);
+				else app.sign_state(app.sign_state1,"q",usr);
+			}
+		},
+		self={on:on,freg:null,
+			reload:function(){
+				this.reset_profile();
+			},
+			reset_profile:function(){
+				var a=app.get_profile();
+				if(a){
+					usr.style.background=a[2];
+				}else{
+					usr.removeAttribute("style");
+				}
+			}
 		};
 		return self;
 	};
